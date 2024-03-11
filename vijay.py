1150c566ff81ae54c1a47f281b5b8a2a58bc16e5
vijay <159439681+Vijaytheanil@users.noreply.github.com>
2024-03-11 17:06:14 +0530

    Add files via upload

---
+++ vijay.py
@@ -0,0 +1,7 @@
+num1=int(input("enter num 1"))
+num2=float(input("enter num 2"))
+print(num1+num2)
+number1=complex(input("enter number1"))
+number2=complex(input("enter number2"))
+print(number1+number2)
+name
